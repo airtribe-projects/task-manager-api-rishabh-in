b47f4f7de5c59fc9925b2dce7e0a5646db3813a7
rishabh <13rishabhtiwari@gmail.com>
2026-05-10 14:50:36 +0530

    added task manager routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const routes = require('./src/routes/task-manager.route')
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes)
 
 app.listen(port, (err) => {
     if (err) {
---
+++ src/routes/task-manager.route.js
@@ -0,0 +1,12 @@
+const {Router} = require('express')
+const router = Router();
+
+router.get('/tasks', handleFetchTasks);
+router.get('/tasks/:id', handleFetchTaskById);
+router.post('tasks', handleAddTasks);
+router.put('/tasks/:id', handleUpdateTask);
+router.patch('/tasks/:id', handlePatchTaskById)
+router.delete('/tasks/:id', handleDeleteTaskById)
+
+
+module.exports = router;
